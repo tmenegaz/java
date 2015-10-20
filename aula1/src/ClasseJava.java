e3cd40c46f30d489215a6ae108cf1c5d74577c9c
tmenegaz <tmeengaz77@gmail.com>
2015-10-20 11:24:07 -0300

    Signed-off-by: tmenegaz <tmeengaz77@gmail.com>

--- aula1/src/ClasseJava.java
+++ aula1/src/ClasseJava.java
@@ -1,23 +1,22 @@
-// comentário de linha
+// comentário de uma linha
 /* comentário
-de muitas
+de
+muitas
 linhas
 */
-/** comentário 
-de muitas linhas
-para a criação de
-documentos que refererenciam a código java.
+/** comentário
+* de
+* muitas
+* linhas
+* para
+* documentação
 */
-/* 1º mandar compilar:
-javac <caminho/arquivo>.java -d ../diretório/
-****************************************************
-para depois executar:
-java -cp ../diretório/ <caminho/nome do arquivo sem a extensão>
-*/
-
 
 class Classe1{
     public static void main (String[] args){
-        System.out.println("Olá, mundo!");
+        System.out.println("Olá mundo!");
     }
 }
+
+
+
