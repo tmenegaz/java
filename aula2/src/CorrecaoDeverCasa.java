590b25ffa54d820adcb45b7b47dbc826e487506e
Tiago Angelo Menegaz <tmenegaz77@gmail.com>
2015-10-20 21:11:31 -0300

    Update CorrecaoDeverCasa.java

--- aula2/src/CorrecaoDeverCasa.java
+++ aula2/src/CorrecaoDeverCasa.java
@@ -1,15 +1,10 @@
 /*
-Criar uma classe chamada animal em um pacote chamado dormitorio,
-dessa classe.
-Criar uma classe cachorro e uma classe gato com pacotes canil
-e gatil.
-
-A classe cachorro e a classe gato deverão, por meio de outra classe,
-servir como base para a criação de objetos.
-
-Uma classe de teste deverá ser criada para verificar se o
-cachorro late e se o gato mia.
+Para corrigir o dever de casa da aula 1 foram criadas mais 4 classes, a saber:
+	Cachorro.java, Gato.java, ObjetoAnimais.java e TesteAnimais.java
+Para compilar
+javac CorrecaoDeverCasa.java -d /classe/
 */
+
 package dormitorio;
 // Faz as vezes da classe animal.
 public class CorrecaoDeverCasa{
