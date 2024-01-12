3be35875e40c371048276512726af064da503c17
tmenegaz <tmenegaz77@gmail.com>
2024-01-12 16:29:38 -0300

    Desafio

---
+++ aula11/desafioList/src/com/mti/agencia/CadastroPacoteViagem.java
@@ -0,0 +1,39 @@
+package com.mti.agencia;
+
+import java.util.List;
+
+public class CadastroPacoteViagem {
+
+    // TODO declarar lista de pacotes de viagem aqui
+
+    public void adicionar(String descricao, double precoDiaria) {
+        // TODO adicionar pacote na lista, validando antes se
+        //  já existe usando contains (lançar exceção)
+    }
+
+    public List<PacoteViagem> obterTodos() {
+        // TODO retornar lista de pacotes
+        return null;
+    }
+
+    public void ordenar() {
+        // TODO ordenar pacotes pela ordem natural
+    }
+
+    public void ordenarPorPrecoDecrescente() {
+        // TODO ordenar pacotes pelo preço (decrescente)
+    }
+
+    public void removerPorDescricao(String descricao) {
+        // TODO iterar nos pacotes com Iterator e remover aqueles com descrição informada,
+        //  lançando exceção se nenhum pacote foi removido
+    }
+
+    public PacoteViagem buscarPorDescricao(String descricao) {
+        // TODO iterar pacotes com enhanced for, localizar e retornar
+        //  pacote com descrição informada (ou lançar exceção se não encontrar)
+        return null;
+    }
+
+}
+
