d442a7b8240da7dbb928dd97da6e8519bf060e90
tmenegaz <tmenegaz77@gmail.com>
2024-01-17 22:20:27 -0300

    Desafio de enumeração

--- aula15/enumeracao/src/App.java
+++ aula15/enumeracao/src/App.java
@@ -1,5 +1,14 @@
+import com.mti.empenho.NotaFiscal;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        NotaFiscal nf = new NotaFiscal(123, "Macbook Pro", 18_000);
+        System.out.println(nf.getDescricaoCompleta());
+
+        nf.emitir();
+        System.out.println(nf.getDescricaoCompleta());
+
+        nf.cancelar(); // IllegalStateException
+        System.out.println(nf.getDescricaoCompleta());
     }
 }
