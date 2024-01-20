2976cc86bcf73ace920c0abb4ccffc180444d965
tmenegaz <tmenegaz77@gmail.com>
2024-01-19 21:34:57 -0300

    regex

--- aula16/strings/src/App.java
+++ aula16/strings/src/App.java
@@ -1,5 +1,9 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) 
+     {
+        String email = "tmenegaz@email.com";
+
+        boolean valido = email.matches("[\\w.-]+@[a-z\\d.-]+\\.[a-z]{2,3}");
+        System.out.println(valido);
     }
 }
